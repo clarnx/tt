4700367c3f8cd46824e82ecce5d9b2becae13086
Alexis Tamayo <lexx426@gmail.com>
2024-02-02 15:23:37 -0600

    Creation of CRUD

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,20 @@
 import Header from './components/Header';
 import Form from './components/Form';
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import List from './components/List';
 
 
 
 function App() {
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
   const [input, setInput] = useState("");
-  const [todos, setToDos] = useState([]);
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <div className='container'>
@@ -18,10 +24,14 @@ function App() {
           input={input}
           setInput={setInput}
           todos = {todos}
-          setToDos= {setToDos}/>
+          setTodos= {setTodos}
+          editTodo = {editTodo}
+          setEditTodo = {setEditTodo}/>
           <List 
             todos = {todos}
-            setToDos= {setToDos}/>
+            setTodos= {setTodos}
+            editTodo = {editTodo}
+          setEditTodo = {setEditTodo}/>
       </div>
       
     </div>
--- todo/src/components/Form.js
+++ todo/src/components/Form.js
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {v4 as uuidv4} from "uuid"
 
-function Form( {input, setInput, todos, setToDos} ) {
+function Form( {input, setInput, todos, setTodos, editTodo, setEditTodo} ) {
+
+    const updateTodo = ( title, id, completed ) => {
+        const newTodo = todos.map((todo) => 
+            todo.id === id ? { title, id, completed } : todo
+        );
+        setTodos( newTodo );
+        setEditTodo(""); 
+    };
+
+    useEffect(() =>{
+        if (editTodo){
+            setInput(editTodo.title);
+        } else {
+            setInput("")
+        }
+    }, [setInput, editTodo]);
+
     const onInputChange = (event) => {
         setInput(event.target.value);
     }
     const onFormSubmit = (event) =>{
         event.preventDefault();
-        setToDos([...todos, {id:uuidv4(), title:input, completed: false}]);
-        setInput("");
-        console.log(setToDos.id + "----")
-        // id doesnt work
-        console.log("+++++++")
-    }
+        if (!editTodo){
+            setTodos([...todos, {id:uuidv4(), title:input, completed: false}]);
+            setInput("");
+        } else {
+            updateTodo(input, editTodo.id, editTodo.completed)
+        }
+    };
+
     return (
         <form onSubmit={onFormSubmit}> 
             <input type='text' placeholder='Enter a chore...'className='task-input' value={input} required onChange={onInputChange}/>
-            <button className='button-add' type='submit'> Add </button>
+            <button className='button-add' type='submit'>
+                { editTodo ? "Edit" : "Add"}
+            </button>
         </form>
     )
 }
